ac598c8130a6e03d97dc003988437976488b37de
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-07-04 09:05:32 +0530

    added prompts from a static file for maintainability

--- src/main/java/com/dailycodebuffer/spring_ai/controller/HelloController.java
+++ src/main/java/com/dailycodebuffer/spring_ai/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.dailycodebuffer.spring_ai.controller;
 
+import java.util.Map;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+
+
 
 @RestController
 public class HelloController {
 
     private final ChatClient chatClient;
+
+    @Value("classpath:/prompts/celeb-details.st")
+    private Resource celebPrompt;
 
     public HelloController(@Autowired ChatClient.Builder builder){
         chatClient = builder.build();
@@ -24,4 +35,12 @@ public class HelloController {
                 .getOutput()
                 .getText();
     }
+
+    @GetMapping("/celeb")
+    public String getCelebDetails(@RequestParam String name) {
+        PromptTemplate template = new PromptTemplate(celebPrompt);
+        Prompt prompt = template.create(Map.of("name", name));
+        return chatClient.prompt(prompt).call().content();
+    }
+    
 }
